f285cf9a68fe138fd83285dd50ce8ef8794f62f4
edwardroag <edwardroag@gmail.com>
2025-12-16 18:47:44 -0500

    Infraestructura DB: conexión MongoDB Atlas y modelo User

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const app = require('./src/app');
 const { PORT } = require('./src/config/env');
+const connectDB = require('./src/config/db');
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Servidor backend corriendo en puerto ${PORT}`);
 });
+
